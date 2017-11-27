3bffafd27385cd57e5277238c420423d3b819841
Alexandre <ale.normand@icloud.com>
2017-11-27 12:46:35 +0100

    add settings admin

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -2,12 +2,13 @@
 <html lang="fr">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="admincss.css">
+    <link rel="stylesheet" href="../style/style.css">
+    <link rel="stylesheet" href="../style/admincss.css">
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Roboto|Spectral+SC" rel="stylesheet">
     <title>Dashboard admin</title>
 </head>
 <body>
@@ -37,7 +38,7 @@
                         <?php if (isset($_SESSION['login'])) { echo $_SESSION['login'];}; ?>
                         <span class="caret"></span></a>
                     <ul class="dropdown-menu" role="menu">
-                        <li class="dropdown-header">OPTIONS</li>
+                        <li class="dropdown-header">OPTION</li>
 <!--                        <li class=""><a href="#">Other Link</a></li>-->
 <!--                        <li class=""><a href="#">Other Link</a></li>-->
 <!--                        <li class=""><a href="#">Other Link</a></li>-->
--- admin/traitement/modifCdt.php
+++ admin/traitement/modifCdt.php
@@ -7,3 +7,15 @@
  */
 
 //UPDATE INFO CANDIDAT à FAIRE..
+session_start();
+include ('../includes/pdo.php');
+
+$sql = "UPDATE utilisateurs SET adresse = :adresse, ville = :ville, email = :email WHERE id = :id";
+$prep = $pdo->prepare($sql);
+$prep->bindValue(':adresse', $_POST['adresse']);
+$prep->bindValue(':ville', $_POST['ville']);
+$prep->bindValue(':email', $_POST['email']);
+$prep->bindValue(':id', $_POST['id'], PDO::PARAM_INT);
+$prep->execute();
+header('Location: ../index.html.php');
+exit();
