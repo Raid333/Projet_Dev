4a9543e4c6973334700a712e7c14cfe32a8e7936
Alexandre Normand <ale.normand@icloud.com>
2017-12-28 13:55:50 +0100

    maj bdd

--- admin/table.sql
+++ admin/table.sql
@@ -3,14 +3,17 @@ CREATE TABLE utilisateurs
 (
   id            INT AUTO_INCREMENT
     PRIMARY KEY,
-  civilite      VARCHAR(255) DEFAULT '''NULL''' NULL,
-  nom           VARCHAR(255) DEFAULT 'NULL'     NULL,
-  prenom        VARCHAR(255) DEFAULT '''NULL''' NULL,
-  adresse       VARCHAR(255) DEFAULT 'NULL'     NULL,
-  codePostal    VARCHAR(50) DEFAULT 'NULL'      NULL,
-  ville         VARCHAR(255) DEFAULT 'NULL'     NULL,
-  dateNaissance INT DEFAULT NULL             NULL,
-  email         VARCHAR(255) DEFAULT 'NULL'     NULL,
-  validation    INT DEFAULT '0'                 NOT NULL,
-  dateInsci     DATE DEFAULT NOW()             NOT NULL
-);
+  civilite      VARCHAR(255) DEFAULT '''''''NULL''''''' NULL,
+  nom           VARCHAR(255) DEFAULT '''NULL'''         NULL,
+  prenom        VARCHAR(255) DEFAULT '''''''NULL''''''' NULL,
+  adresse       VARCHAR(255) DEFAULT '''NULL'''         NULL,
+  codePostal    VARCHAR(50) DEFAULT '''NULL'''          NULL,
+  ville         VARCHAR(255) DEFAULT '''NULL'''         NULL,
+  dateNaissance INT DEFAULT 'NULL'                      NULL,
+  email         VARCHAR(255) DEFAULT '''NULL'''         NULL,
+  validation    INT DEFAULT '0'                         NOT NULL,
+  dateInsci     DATE DEFAULT 'current_timestamp()'      NOT NULL,
+  codeBarre     INT(12)                                 NULL
+)
+  ENGINE = InnoDB;
+
