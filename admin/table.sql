3199b769bd9d27e6d4592b730a7864bfda85b0d0
Alexandre Normand <ale.normand@icloud.com>
2018-01-06 21:33:57 +0100

    maj sql

--- admin/table.sql
+++ admin/table.sql
@@ -1,16 +1,17 @@
--- auto-generated definition
 CREATE TABLE utilisateurs
 (
   id            INT AUTO_INCREMENT
     PRIMARY KEY,
-  civilite      VARCHAR(255) DEFAULT '''NULL''' NULL,
-  nom           VARCHAR(255) DEFAULT 'NULL'     NULL,
-  prenom        VARCHAR(255) DEFAULT '''NULL''' NULL,
-  adresse       VARCHAR(255) DEFAULT 'NULL'     NULL,
-  codePostal    VARCHAR(50) DEFAULT 'NULL'      NULL,
-  ville         VARCHAR(255) DEFAULT 'NULL'     NULL,
-  dateNaissance INT DEFAULT NULL             NULL,
-  email         VARCHAR(255) DEFAULT 'NULL'     NULL,
-  validation    INT DEFAULT '0'                 NOT NULL,
-  dateInsci     DATE DEFAULT NOW()             NOT NULL
-);
+  civilite      VARCHAR(255) DEFAULT '''''''NULL''''''' NULL,
+  nom           VARCHAR(255) DEFAULT '''NULL'''         NULL,
+  prenom        VARCHAR(255) DEFAULT '''''''NULL''''''' NULL,
+  adresse       VARCHAR(255) DEFAULT '''NULL'''         NULL,
+  codePostal    VARCHAR(50) DEFAULT '''NULL'''          NULL,
+  ville         VARCHAR(255) DEFAULT '''NULL'''         NULL,
+  dateNaissance INT DEFAULT 'NULL'                      NULL,
+  email         VARCHAR(255) DEFAULT '''NULL'''         NULL,
+  validation    INT DEFAULT '0'                         NOT NULL,
+  dateInsci     DATE DEFAULT '''0000-00-00'''           NOT NULL,
+  codeBarre     VARCHAR(15) DEFAULT '''0'''             NULL
+)
+  ENGINE = InnoDB;
